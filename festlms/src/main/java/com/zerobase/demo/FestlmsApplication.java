7ee362c763664026e71a08e5be632f0145885338
Son <seokson1@nave.com>
2023-11-05 01:32:15 +0900

    gethub test

---
+++ festlms/src/main/java/com/zerobase/demo/FestlmsApplication.java
@@ -0,0 +1,13 @@
+package com.zerobase.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FestlmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FestlmsApplication.class, args);
+	}
+
+}
